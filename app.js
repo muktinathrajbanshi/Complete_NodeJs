0ea5356a5867d48b659b6100f67ecc014de6e4ae
unknown <muktinathrajbanshi786@gmail.com>
2025-05-04 17:38:30 +0545

    Node.js Secrets

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log(global);
+global.console.log("Hello, Muktinath How are You");
+globalThis.console.log("Hello, Muktinath are you fine");
+
+
+// globalThis.console.log(module);
+console.log(globalThis.module);
+//* commonjs
+// console.log(process);
+//! console.log(window.document); //js
+
+
+// React - components
+// Node - module
