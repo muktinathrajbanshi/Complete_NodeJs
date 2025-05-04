2eccc6b54ab0162b8dd709331d30bda9be4d2e06
unknown <muktinathrajbanshi786@gmail.com>
2025-05-04 19:06:52 +0545

    Node.js Modules

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-// console.log(global);
-global.console.log("Hello, Muktinath How are You");
-globalThis.console.log("Hello, Muktinath are you fine");
+const add = require("./math");
 
+console.log(add(5, 10));
 
-// globalThis.console.log(module);
-console.log(globalThis.module);
-//* commonjs
-// console.log(process);
-//! console.log(window.document); //js
-
-
-// React - components
-// Node - module
+// common js
