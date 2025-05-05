ef35dc28169f710ae5fe2cc88f6fd589f8e121d6
unknown <muktinathrajbanshi786@gmail.com>
2025-05-05 22:58:59 +0545

    Node.js FS Module with Promises: Write, Read, Update & Delete Files Easily

--- crypto.js
+++ crypto.js
@@ -7,5 +7,7 @@ console.log(randomValue);
 const hashValue = crypto.createHash("sha256").update("muktinath rajbanshi").digest("hex");
 console.log(hashValue);
 
-const hashValue1 = crypto.createHash("sha256").update("muktinath rajbanshi").digest("hex");
-console.log(hashValue1);
+// const hashValue1 = crypto.createHash("sha256").update("muktinath rajbanshi").digest("hex");
+// console.log(hashValue1);
+
+//* 192ff5a89ff433098a46aabc43d1353f0f4af0f8316af8af556fdb9483114c3f
