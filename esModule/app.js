b0f5f5ee0d1f0486b612336fda5b3d8bdd08a4a9
unknown <muktinathrajbanshi786@gmail.com>
2025-05-06 23:35:27 +0545

    ESModules in Node.js: Default, Named, and Aliased Imports Explained!

---
+++ esModule/app.js
@@ -0,0 +1,38 @@
+// const add = require("./math");
+
+// const { add, mul, sub, div, PI } = require("./math");
+
+// console.log(add(5, 10));
+// console.log(mul(5, 10));
+// console.log(sub(5, 10));
+// console.log(div(5, 10));
+// console.log(PI);
+
+// const mul = require("./math");
+// const math = require("./math");
+// import multiply from "./math.js";
+import { add, sub, mult, div, PI} from "./math.js";
+import * as math from "./math.js";
+
+
+console.log(math.add(5, 10));
+console.log(mult(70, 10));
+console.log(math.sub(5, 10));
+console.log(div(50, 10));
+console.log(math.PI);
+
+
+
+
+
+// console.log("Hello, World!");
+
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.map((num) => num * 5));
+
+
+
+
+
+
---
+++ esModule/math.js
@@ -0,0 +1,22 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const sub = (a, b) => {
+    return a - b;
+};
+
+const div = (a, b) => {
+    return a / b;
+};
+
+
+const mult = (a, b) => {
+    return a * b;
+};
+
+const PI = 3.214;
+
+// export default mul;
+
+export { add, sub, mult, div, PI};
