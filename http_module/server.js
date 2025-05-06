d258b4282d47829479333028a9b55d8946997d06
unknown <muktinathrajbanshi786@gmail.com>
2025-05-06 19:20:30 +0545

    Creating a Web Server in Node.js : HTTP Module in Node JS

---
+++ http_module/server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+//web server
+const server = http.createServer((req, res) => {
+    if(req.url === "/") {
+        res.setHeader("Contact-Type", "text/html");
+        res.write("<h1>Hello everyone this is me Muktinath Rajbanshi!</h1>");
+        res.end();
+    }
+
+    if(req.url === "/source-code") {
+        res.write("Please visit my youtube channel!");
+        res.end();
+    }
+
+    if(req.url === "/contact") {
+        res.setHeader("Contact-Type", "text/plain");
+        res.write("This is my contact section!");
+        res.end();
+    }
+
+});
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`🔥 Listening on PORT ${PORT}`);
+});
