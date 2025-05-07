a0dc908ebee3c3a289f71038e3acc583d42d8b50
unknown <muktinathrajbanshi786@gmail.com>
2025-05-07 16:27:24 +0545

    NPM Commands: Install, Update, and Manage Node.js Packages

--- esModule/app.js
+++ esModule/app.js
@@ -40,3 +40,30 @@ console.log(math.PI);
 // 0. Patch Version (Small request or bug fix, e.g. update or edit existing elements)
 
 
+// npm install <package-name> @ <version>
+// examples:
+// npm install express@4.0.0 // ^4.0.0
+// npm install express@4.0.0 --save-exact // To install exact vesion.
+// npm install express@~4.0.0
+// npm install express@4.2.x
+// You can use any symbols while specifying version.
+
+// npm outdated
+// It shows outdated packages in your project.
+// Current: The version of the package currently installed in your project.
+// Wanted: The latest version that specifies the version range defined in your package.json. It basically uses the symbols to get it.
+// Latest: Absolute latest version of the package.
+
+// npm update
+
+// npm remove <package-name>
+
+
+// npx npm-check-updates 
+// npx is a CLI tool that comes with npm.
+// It is used to execute a package without requiring you to install globally or locally.
+// It is useful for temporary usage of a package.
+// npm-check-updates is a package which you can use to upgrade your packages to absolute latest versions.
+// Use -u flag at the end to update the packages after reviewing.
+// This only updates package.json, then you can use npm install to update the packages.
+
