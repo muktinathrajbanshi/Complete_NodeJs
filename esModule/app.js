1d49d9841dc4d1a37c9c652522ca6d9b854f6723
unknown <muktinathrajbanshi786@gmail.com>
2025-05-07 16:47:09 +0545

    Global NPM Packages Explained: Manage, Update, and Remove Easily! From here NPM Part Completed

--- esModule/app.js
+++ esModule/app.js
@@ -67,3 +67,17 @@ console.log(math.PI);
 // Use -u flag at the end to update the packages after reviewing.
 // This only updates package.json, then you can use npm install to update the packages.
 
+
+
+// Global Packages
+// Up until now, we have been installing packages locally.
+// To install packages globally,
+// npm install -g npm-check-updates
+// On Linux, and macOS, you might have to prefix it with sudo
+// This package gets installed globally, and you can access from anywhere.
+// You can try running npm-check-updates command anywhere in any projects without using npx.
+// npm-check-updates also have an alias named ncu, which you can also use.
+// Fun fact: npm is itself a global package which means if you need to update npm, you will
+// do: npm install -g npm
+
+
