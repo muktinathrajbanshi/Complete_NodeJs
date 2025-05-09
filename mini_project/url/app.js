d0dfaac2d0a4ff7d93347915ebc22481503b6bb1
unknown <muktinathrajbanshi786@gmail.com>
2025-05-09 22:41:00 +0545

    Final Part Build URL Shortener with Node.js - Truncate Text & Redirect Logic

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -45,11 +45,21 @@ const server = createServer(async (req, res) => {
             return serveFile(res, path.join("public", "index.html"), "text/html");
          } else if (req.url === "/style.css") {
             return serveFile(res, path.join("public", "style.css"), "text/css");
-            } else if(req.url === "/links"){
+            } else  if(req.url === "/links"){
               const links = await loadLinks();
-
               res.writeHead(200, {"Content-Type": "application/json" });
               return res.end(JSON.stringify(links));
+            } else {
+                const links = await loadLinks();
+                const shortCode = req.url.slice(1);
+                console.log("links red. ", req.url);
+                
+                if(links[shortCode]) {
+                    res.writeHead(302, {location : links[shortCode]});
+                    return res.end();
+                }
+                res.writeHead(404, { "Content-Type" : "text/plain" });
+                return res.end("Shortened URL is not found");
             }
         }
 
